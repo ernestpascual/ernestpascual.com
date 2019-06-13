d22fb62d0433ab53ea358a5549c4982d0facf60c
ernestpascual <eepascual@gmail.com>
2019-06-13 20:09:40 +0800

    fixed typo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home"
-import Resume from "./components/Resume"
-
 
 class App extends Component {
-  render() {w
+  render() {
     return (
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
-
         </Switch>
       </div>
     );
